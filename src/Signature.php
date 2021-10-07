b166a951f98a4d3582d695f9f7a923d6170cd7b0
Andrey Mistulov <a.mistulov@aristos.pw>
2021-10-07 12:27:19 +0300

    Init version + tests

---
+++ src/Signature.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Prowebcraft\PhpRequestSignature;
+
+/**
+ * Class for signing and verifying requests.
+ * User: Andrey Mistulov
+ */
+class Signature
+{
+
+    protected string $salt = '';
+
+    /**
+     * @param string $salt
+     */
+    public function __construct(string $salt)
+    {
+        $this->salt = $salt;
+    }
+
+    /**
+     * Sign path and params
+     * @param string $path
+     * @param array $params
+     * @return string
+     */
+    public function sign(string $path = '', array $params = []): string
+    {
+        $res = [];
+        foreach ($params as $key => $val) {
+            if (strtolower($key) === 'signature') {
+                continue;
+            }
+            if (is_array($val)) {
+                $res[] = $key . json_encode($val);
+            } else {
+                $res[] = $key . $val;
+            }
+        }
+        sort($res);
+        $signString = $path . implode('', $res);
+        return strtoupper(bin2hex(hash_hmac("sha1", $signString, $this->salt, true)));
+    }
+
+    /**
+     * Check incoming request with signature
+     * @param string $signature
+     * @param string $path
+     * @param array $params
+     * @return bool
+     */
+    public function check(string $signature, string $path = '', array $params = []): bool
+    {
+        return $this->sign($path, $params) === $signature;
+    }
+}
---
+++ tests/unit/SignatureTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Prowebcraft\PhpRequestSignature\Signature;
+
+class SignatureTest extends TestCase
+{
+
+    /**
+     * Test Signature and check
+     * @dataProvider sets
+     */
+    public function testCheckSignature(string $title, string $path, array $params, string $expectedSignature)
+    {
+        $sender = new Signature('TEST');
+        $generatedSignature = $sender->sign($path, $params);
+        self::assertEquals($expectedSignature, $generatedSignature, $title);
+        $receiver = new Signature('TEST');
+        $receiverParams = json_decode(json_encode($params), true);
+        self::assertTrue($receiver->check($expectedSignature, $path, $receiverParams));
+    }
+
+    /**
+     * Test Salt integrity
+     */
+    public function testSalt()
+    {
+        $sender = new Signature('TEST');
+        $path = '/test';
+        $params = [];
+        $senderSignature = $sender->sign($path, $params);
+        $receiver = new Signature('BROKEN_SALT');
+        self::assertFalse($receiver->check($senderSignature, $path, $params));
+    }
+
+    /**
+     * Dataset for test
+     * @return array[]
+     */
+    public function sets(): array
+    {
+        return [
+            [ 'simple string param (no path)', '', [ 'simple' => 'hello' ], '381879132F73837C5A23FA908D42A8AE332E730B'],
+            [ 'simple string param (with path)', '/test/path', [ 'simple' => 'hello' ], '875258845E685F9EA5B0559042EDF8326B71B365'],
+            [ 'complex array', '/test/path', [
+                'simple' => 'hello',
+                'nested_array' => [
+                    'hello' => [
+                        'world'
+                    ]
+                ],
+                'a' => null,
+                'b' => false,
+                0 => true
+            ], '69CA087C84A299DD04B85769A026B6F9F54DB7EF'],
+        ];
+    }
+}
